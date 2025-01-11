871995411f5994b27659660df1a47ae04e5d6cd5
MJedrychowska <magdalena.jedrychowska@student.up.krakow.pl>
2025-01-11 18:36:01 +0100

    .

--- Biblioteka/Models/ErrorViewModel.cs
+++ Biblioteka/Models/ErrorViewModel.cs
@@ -2,6 +2,7 @@ namespace Biblioteka.Models
 {
     public class ErrorViewModel
     {
+        int x = 0;
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
